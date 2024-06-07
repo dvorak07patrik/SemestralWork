b7befccc2cab184cd7b919dba3c7f2fcff280921
Patrik Dvorak <dvorak07patrik@seznam.cz>
2024-06-07 20:45:03 +0200

    add script for testing database connection

---
+++ connectionTest.py
@@ -0,0 +1,23 @@
+import psycopg2
+
+try:
+    # Establish connection to PostgreSQL
+    connection = psycopg2.connect(
+        user="postgres",
+        password="mysecretpassword",
+        host="localhost",
+        port="5432",
+        database="postgres"
+    )
+
+    cursor = connection.cursor()
+    # Execute a test query
+    cursor.execute("SELECT version();")
+    record = cursor.fetchone()
+    print(f"You are connected to - {record}\n")
+
+    # Close the cursor and connection
+    cursor.close()
+    connection.close()
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
